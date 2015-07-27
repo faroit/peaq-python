a0d7678db62bab2a3ff807a22e85c37328fd9f1f
dinhngoctu <dinhngoctu@localhost>
2015-07-27 17:46:04 +0200

    custom options support + api changes

--- peaq.h
+++ peaq.h
@@ -54,7 +54,31 @@ PQ_InitMOVC (struct PQ_MOVBC *MOVC, int Nchan, int Np)
   assert (xp - x == N);
 }
 
-void peaqc_setopt(struct PQ_Opt *opt) {
+struct peaqc_vars {
+    double FS;  /* Sampling frequency (double) */
+    int NF;  /* Frame length */
+    int CB_ADV;  /* Frame advance */
+    int FB_ADV;  /* Sub-sampling factor in filter bank */
+    double FCS;  /* Test sine frequency */
+    double AMAX;  /* Max. input level corresponding to the max. listening level */
+    int NL;  /* Number of correlation lags (EHS MOV) */
+    int MAXNC;  /* Maximum number of channels */
+    int NMOV_MAX;
+};
+
+void peaqc_getvars(struct peaqc_vars *vars) {
+    vars->FS = PQ_FS;
+    vars->NF = PQ_NF;
+    vars->CB_ADV = PQ_CB_ADV;
+    vars->FB_ADV = PQ_FB_ADV;
+    vars->FCS = PQ_FCS;
+    vars->AMAX = PQ_AMAX;
+    vars->NL = PQ_NL;
+    vars->MAXNC = PQ_MAXNC;
+    vars->NMOV_MAX = PQ_NMOV_MAX;
+}
+
+void peaqc_defaultopt(struct PQ_Opt *opt) {
     opt->Lp = 92.;
     opt->Ni = 0;
     opt->ClipMOV = 0;
@@ -71,40 +95,30 @@ struct peaqc_result {
     double ODG;
 };
 
-struct peaqc_result peaqc_eval(const double *xr, const double *xt, size_t nsamp) {
-//double peaqc_eval(const double *xr, const double *xt, size_t nsamp) {
-    struct PQ_Opt opt;
-    peaqc_setopt(&opt);
-
+void peaqc_eval(struct peaqc_result *results, const double *xr, const double *xt, size_t nsamp, struct PQ_Opt *opt) {
     size_t Np = nsamp / PQ_CB_ADV;
 
     struct PQ_MOVBC MOVC;
     PQ_InitMOVC(&MOVC, 1, Np);
-    MOVC.PD.c1 = opt.PDfactor;
+    MOVC.PD.c1 = opt->PDfactor;
 
     struct PQ_Par par;
-    PQgenTables(0, &opt, &par);
+    PQgenTables(0, opt, &par);
 
     struct PQ_FiltMem fmem;
-    PQinitFMem(par.CB.Nc, opt.PCinit, &fmem);
+    PQinitFMem(par.CB.Nc, opt->PCinit, &fmem);
 
     for (int i = 0; i < Np; i++) {
         struct PQ_MOVBI movbi = PQeval(xr+i*PQ_CB_ADV, xt+i*PQ_CB_ADV, &par, &fmem);
         PQframeMOVB(&movbi, 1, i, &MOVC);
 
-        /*
-        if (i % 50 == 0) {
+        if (opt->Ni != 0 && i % opt->Ni == 0) {
             PQprtMOVCi (1, i, &MOVC);
         }
-        */
     }
 
-    struct peaqc_result results;
+    PQavgMOVB(&MOVC, 1, 0, Np, results->MOV);
 
-    PQavgMOVB(&MOVC, 1, 0, Np, results.MOV);
-
-    double ODG = PQnNet(results.MOV, &(par.NNet), &(results.DI));
-    results.ODG = ODG;
-
-    return results;
+    double ODG = PQnNet(results->MOV, &(par.NNet), &(results->DI));
+    results->ODG = ODG;
 }
--- peaq.py
+++ peaq.py
@@ -40,13 +40,28 @@ def pqeval(reference: np.ndarray, target: np.ndarray):
     return list(MOV), di[0], ODG
 '''
 
-def pqeval(reference: np.ndarray, target: np.ndarray, scale=32768.):
+def pqvars():
+    vars = ffi.new('struct peaqc_vars *')
+    lib.peaqc_getvars(vars)
+    return vars
+
+def pqopt():
+    opt = ffi.new('struct PQ_Opt *')
+    lib.peaqc_defaultopt(opt)
+    return opt
+
+def pqeval(reference: np.ndarray, target: np.ndarray, scale, options=None):
+    if options is None:
+        options = pqopt()
+
     xr = reference.flatten().astype(dtype=np.float64)
     xr *= scale
     xt = target.flatten().astype(dtype=np.float64)
     xt *= scale
 
-    pr = ffi.cast('double *', xr.ctypes.data)
-    pt = ffi.cast('double *', xt.ctypes.data)
+    pr = ffi.cast('double const *', xr.ctypes.data)
+    pt = ffi.cast('double const *', xt.ctypes.data)
 
-    return lib.peaqc_eval(pr, pt, min(xr.size, xt.size)).ODG
+    results = ffi.new('struct peaqc_result *')
+    lib.peaqc_eval(results, pr, pt, min(xr.size, xt.size), options)
+    return results.ODG
