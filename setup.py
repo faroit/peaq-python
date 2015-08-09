900afc79046355117e17fd27efa1e900b8b89051
dinhngoctu <dinhngoctu@localhost>
2015-08-09 19:55:04 +0200

    polish

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=['peaq'],
     ext_package='peaq',
     setup_requires=['cffi>=1.0.0'],
-    install_requires=['cffi>=1.0.0'],
+    install_requires=['numpy>=1.8', 'cffi>=1.0.0'],
     cffi_modules=['_cffi_build/peaq_build.py:ffi']
 )
